bdc106f636a53e87ce5b3c3b4b87e144eb0c8e8d
gabrijela gavric and qudsia ahmad <gabrijela+qudsia@>
2017-10-19 16:50:04 -0700

    working .env file

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -1,11 +1,11 @@
 import {Bike } from './../js/weather';
-
+let apiKey = require('./../js/weather2.js').bikeModule;
 $(document).ready(function() {
   let bike = new Bike();
   $('#weatherLocation').click(function() {
     let city = $('#location').val();
     $('#location').val("");
-    let promise = bike.makePromise(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=eeb72cd89e66872719c274878618bb7a`);
+    let promise = bike.makePromise(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=apiKey`);
     bike.callApi(promise,city);
   });
 });
